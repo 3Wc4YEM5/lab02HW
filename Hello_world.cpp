1da44bd5997a99e920ef391ff5465536e52ea82d
Pupkenberg <113918951+3Wc4YEM5@users.noreply.github.com>
2024-05-02 14:23:44 +0300

    new comment Hello_world.cpp

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -2,6 +2,6 @@
 int main() { 
     std::string name;
     std::cout << "Enter name "; 
-    std::cin >> name; //Запрос @name
+    std::cin >> name; //Запрос @name для программы
     std::cout << "Hello world from " << name;
 }
