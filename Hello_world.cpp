a7fd39a6ac664e2a0e2e7ec2915a1d542b970d51
3Wc4YEM5 <aibushev.goscha@yandex.ru>
2024-05-02 14:00:21 +0300

    patched  Hello_world.cpp

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-using namespace std;
 int main() { 
-    string name;
-    cout << "Enter name ";
-    cin >> name;
-    cout << "Hello world from " << name;
+    std::string name;
+    std::cout << "Enter name ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name;
 }
